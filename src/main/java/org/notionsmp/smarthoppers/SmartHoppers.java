080ed0f61cc4c7178909c7909caaf4e64469cc98
Richard Mikloš <akisytbsteamy@gmail.com>
2025-04-25 23:06:20 +0200

    feat: initial commit

---
+++ src/main/java/org/notionsmp/smarthoppers/SmartHoppers.java
@@ -0,0 +1,17 @@
+package org.notionsmp.smarthoppers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmartHoppers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

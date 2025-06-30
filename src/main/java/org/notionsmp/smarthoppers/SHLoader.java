3f5388d74249a12fa9a3e77f8bdb6072ba28cff2
Richard Mikloš <akisytbsteamy@gmail.com>
2025-06-30 12:16:03 +0200

    fix: use maven central mirror when available to comply with tos

--- src/main/java/org/notionsmp/smarthoppers/SHLoader.java
+++ src/main/java/org/notionsmp/smarthoppers/SHLoader.java
@@ -7,6 +7,7 @@ import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class SHLoader implements PluginLoader {
@@ -21,7 +22,16 @@ public class SHLoader implements PluginLoader {
 
     MavenLibraryResolver resolver = new MavenLibraryResolver();
 
-    resolver.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/").build());
+    try {
+      Field mirrorField = MavenLibraryResolver.class.getField("MAVEN_CENTRAL_DEFAULT_MIRROR");
+      Object mirrorRepo = mirrorField.get(null);
+      if (mirrorRepo instanceof RemoteRepository) {
+        resolver.addRepository((RemoteRepository) mirrorRepo);
+      }
+      SmartHoppers.getInstance().getLogger().info("MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR found, using it to comply with ToS of the Maven Central");
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      resolver.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/").build());
+    }
 
     for (String dep : DEPENDS) {
       resolver.addDependency(new Dependency(new DefaultArtifact(dep), null));
